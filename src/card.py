ef07b10772fa9d2e604386cb7cc8272079204861
yosef-mercado <yosef.mercado@outlook.com>
2022-09-22 17:33:50 +0800

    initial commit; created base assignment file structure; begun work on Card and Character classes

---
+++ src/card.py
@@ -0,0 +1,6 @@
+class Card():
+    def __init__(self, name, rarity, cost, effect):
+        self.name = name
+        self.rarity = rarity
+        self.cost = cost
+        self.effect = effect
---
+++ src/character.py
@@ -0,0 +1,18 @@
+class Character():
+    def __init__(self, name, hp, shield, dice, deck):
+        self.name = name
+        self.hp = hp
+        self.current_hp = hp
+        self.shield = shield
+        self.current_shield = shield
+        self.dice = dice
+        self.deck = deck
+
+class Player(Character):
+    def __init__(self, name, job, hp, shield, dice, deck, deck_size):
+        super().__init__(name, hp, shield, dice, deck)
+        self.job = job
+        self.deck_size = deck_size
+
+class Enemy(Character):
+    pass
