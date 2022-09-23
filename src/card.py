a87cb9f4bd7b8046fe5e87cabd665d9971f7752f
yosef-mercado <yosef.mercado@outlook.com>
2022-09-24 02:58:18 +0800

    completed and tested random_card_name() function

--- src/card.py
+++ src/card.py
@@ -1,30 +1,7 @@
 import random
 
+from character import Job
 from enum import Enum
-
-class Rarity(Enum):
-    UNIQUE = "Unique"
-    COMMON = "Common"
-    RARE = "Rare"
-    SUPER_RARE = "Super Rare"
-
-class Card():
-    def __init__(self, name, rarity, cost, effect):
-        self.name = name
-        self.rarity = rarity
-        self.cost = cost
-        self.effect = effect
-
-WEAPON_ADJECTIVES = (
-    "Ordinary", "Magical",
-    "Brand New", "Antiquated",
-    "Authentic", "Fake",
-    "Shiny", "Rusted",
-    "Angelic", "Hellish",
-    )
-FIGHTER_WEAPONS = ("Axe", "Mace", "Sword")
-MAGE_WEAPONS = ("Rod", "Staff", "Wand")
-THIEF_WEAPONS = ("Crossbow", "Dagger", "Short Sword")
 
 COSTS = {
     "Any": [1, 2, 3, 4, 5, 6],
@@ -45,3 +22,39 @@ COSTS = {
     "Max5": [1, 2, 3, 4, 5],
     "6": 6,
     }
+
+class Rarity(Enum):
+    UNIQUE = "Unique"
+    COMMON = "Common"
+    RARE = "Rare"
+    SUPER_RARE = "Super Rare"
+
+class Card():
+    def __init__(self, name, rarity, cost, effect):
+        self.name = name
+        self.rarity = rarity
+        self.cost = cost
+        self.effect = effect
+
+def random_card_name(player_job):
+    ADJECTIVES = (
+        "Ordinary", "Magical",
+        "Brand New", "Antiquated",
+        "Authentic", "Fake",
+        "Shiny", "Rusted",
+        "Angelic", "Hellish",
+        )
+    FIGHTER_WEAPONS = ("Axe", "Mace", "Sword")
+    MAGE_WEAPONS = ("Rod", "Staff", "Wand")
+    THIEF_WEAPONS = ("Crossbow", "Dagger", "Short Sword")
+
+    adjective = random.choice(ADJECTIVES)
+
+    if player_job == Job.FIGHTER:
+        weapon = random.choice(FIGHTER_WEAPONS)
+    elif player_job == Job.MAGE:
+        weapon = random.choice(MAGE_WEAPONS)
+    elif player_job == Job.THIEF:
+        weapon = random.choice(THIEF_WEAPONS)
+
+    return adjective + " " + weapon
