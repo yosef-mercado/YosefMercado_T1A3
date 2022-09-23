5f39a98b7b5df065b482e5cce4d4826d50148187
yosef-mercado <yosef.mercado@outlook.com>
2022-09-24 00:05:08 +0800

    begun implementation of Card rarities and Card costs

--- src/card.py
+++ src/card.py
@@ -1,6 +1,29 @@
+import random
+
+rarities = [
+    "Unique",
+    "Common",
+    "Rare",
+    "Super Rare"
+    ]
+
+x = random.randint(1, 6)
+
+costs = {
+    "Any": [1, 2, 3, 4, 5, 6],
+    "Odd": [1, 3, 5],
+    "Even": [2, 4, 6],
+    str(x): x,
+    "Min " + str(x): [],
+    "Max " + str(x): []
+    }
+
 class Card():
     def __init__(self, name, rarity, cost, effect):
         self.name = name
         self.rarity = rarity
         self.cost = cost
         self.effect = effect
+
+
+print(costs)
