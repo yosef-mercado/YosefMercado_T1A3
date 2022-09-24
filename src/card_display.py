23188c13f9f52cb84214bc2bc861ff72c26e3add
yosef-mercado <yosef.mercado@outlook.com>
2022-09-24 09:42:11 +0800

    created card_display module to read card values and display as player-readable information; tested and currently performing as expected

---
+++ src/card_display.py
@@ -0,0 +1,50 @@
+from card import Cost
+from card import Rarity
+from card import Effect
+
+read_cost = {
+    Cost.ANY: "Any",
+    Cost.ODD: "Odd",
+    Cost.EVEN: "Even",
+    Cost.ONE: "1",
+    Cost.TWO: "2",
+    Cost.MIN_TWO: "MIN 2",
+    Cost.MAX_TWO: "MAX 2",
+    Cost.THREE: "3",
+    Cost.MIN_THREE: "MIN 3",
+    Cost.MAX_THREE: "MAX 3",
+    Cost.FOUR: "4",
+    Cost.MIN_FOUR: "MIN 4",
+    Cost.MAX_FOUR: "MAX 4",
+    Cost.FIVE: "5",
+    Cost.MIN_FIVE: "MIN 5",
+    Cost.MAX_FIVE: "MAX 5",
+    Cost.SIX: "6"
+    }
+
+read_rarity = {
+    Rarity.COMMON: "Common",
+    Rarity.RARE: "Rare",
+    Rarity.SUPER_RARE: "Super Rare"
+}
+
+def display_card(card):
+    print("[" + read_rarity[card.rarity] + "]" + " " + card.name)
+    print("Cost:")
+    print(read_cost[card.cost])
+    display_effect(card)
+
+def display_effect(card):
+    print("Effect: ")
+    
+    for effect in card.effect:
+        if effect[0] == Effect.DAMAGE:
+            print("Deal " + str(effect[1]) + " damage")
+        elif effect[0] == Effect.HEAL:
+            print("Restore " + str(effect[1]) + " HP")
+        elif effect[0] == Effect.SHIELD:
+            print("Gain " + str(effect[1]) + " Shield")
+        elif effect[0] == Effect.DICE and effect[1] == "same":
+            print("Return dice with the same value")
+        elif effect[0] == Effect.DICE and effect[1] == "new":
+            print("Return dice with a new value")
