1749c2d4a54a10be8a8e0595d6549a3982eb2e75
yosef-mercado <yosef.mercado@outlook.com>
2022-09-24 15:49:28 +0800

    minor comment syntax adjustments

--- src/player_card_list.py
+++ src/player_card_list.py
@@ -4,7 +4,7 @@ from card import Effect
 from card import Rarity
 
 # unique preset Cards for the Fighter job
-# Fighter job card 1: Adventurer's Sword
+# Fighter Card 1: Adventurer's Sword
 fighter_card_1 = Card(
     "Adventurer's Sword",
     Rarity.SUPER_RARE,
@@ -13,7 +13,7 @@ fighter_card_1 = Card(
 
 fighter_card_1.add_effect([Effect.DAMAGE_2, Effect.DAMAGE_2.value[0]])
 
-# Fighter job card 2: Adventurer's Potion
+# Fighter Card 2: Adventurer's Potion
 fighter_card_2 = Card(
     "Adventurer's Potion",
     Rarity.SUPER_RARE,
@@ -23,7 +23,7 @@ fighter_card_2 = Card(
 fighter_card_2.add_effect([Effect.HEAL, Effect.HEAL.value[1]])
 
 # unique preset Cards for the Mage job
-# Mage job card 1: Adventurer's Wand
+# Mage Card 1: Adventurer's Wand
 mage_card_1 = Card(
     "Adventurer's Wand",
     Rarity.SUPER_RARE,
@@ -32,7 +32,7 @@ mage_card_1 = Card(
 
 mage_card_1.add_effect([Effect.DAMAGE_1, Effect.DAMAGE_1.value[1]])
 
-# Mage job card 1: Adventurer's Fireball
+# Mage Card 1: Adventurer's Fireball
 mage_card_2 = Card(
     "Adventurer's Fireball",
     Rarity.SUPER_RARE,
@@ -42,7 +42,7 @@ mage_card_2 = Card(
 mage_card_2.add_effect([Effect.DAMAGE_1, Effect.DAMAGE_1.value[3]])
 
 # unique preset Cards for the Thief job
-# Thief job card 1: Adventurer's Dagger
+# Thief Card 1: Adventurer's Dagger
 thief_card_1 = Card(
     "Adventurer's Dagger",
     Rarity.SUPER_RARE,
@@ -51,7 +51,7 @@ thief_card_1 = Card(
 
 thief_card_1.add_effect([Effect.DAMAGE_1, Effect.DAMAGE_1.value[3]])
 
-# Thief job card 2: Adventurer's Rock
+# Thief Card 2: Adventurer's Rock
 thief_card_2 = Card(
     "Adventurer's Rock",
     Rarity.SUPER_RARE,
